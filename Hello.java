7efb0398eed4b74710177ee2b20cfbd8990831e0
AayuAmor <aayuamor@gmail.com>
2025-09-01 07:57:55 +0545

    Refactor Hello.java to uncomment and organize example programs

--- Hello.java
+++ Hello.java
@@ -1,38 +1,44 @@
-// public class Hello {
-//     public static void main(String[] args) {
-//         System.out.print("Hello World!\n");
-//         System.out.print("This is My First Program\nI am on module ST4003CEM\n");
-//     }
-// }
+/* 
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("Hello World!\n");
+        System.out.print("This is My First Program\nI am on module ST4003CEM\n");
+    }
+}
+*/
 
 // Question no. 3
-
-// public class Hello{
-//     public static void main(String[] args) {
-//         System.out.print("A \"quoted\" String is\n 'much' better if you learn\n the rules of \"escape sequences.\" \n Also, \"\" represents an empty\n String. Don't forget: use \\\"\ninstead of \" !\n '' is not the same as \"");
-//     }
-// }
+/* 
+public class Hello{
+    public static void main(String[] args) {
+        System.out.print("A \"quoted\" String is\n 'much' better if you learn\n the rules of \"escape sequences.\" \n Also, \"\" represents an empty\n String. Don't forget: use \\\"\ninstead of \" !\n '' is not the same as \"");
+    }
+}
+*/
 
 // 4 no. question
-
-// public class Hello{
-//     public static void main(String[] args) {
-//         System.out.print("*\n**\n***\n****\n*****");
-//     }
-// }
+/*
+public class Hello{
+    public static void main(String[] args) {
+        System.out.print("*\n**\n***\n****\n*****");
+    }
+}
+*/
 
 // 5 no. question
-
-// public class Hello{
-//     public static void main(String[] args) {
-//         System.out.print("*********\n*********\n*********\n*********\n*********");
-//     }
-// }
+/*
+public class Hello{
+    public static void main(String[] args) {
+        System.out.print("*********\n*********\n*********\n*********\n*********");
+    }
+} */
 
 // 6 no. question
 
+/*
 public class Hello{
     public static void main(String[] args) {
         System.out.print("A well-formed Java\nprogram has a main\nmethod with { and } braces.\r\nA System.out.println\nstatement has ( and ) and\nusually a String that starts\nand ends with a \"\ncharacter.\r\n(But we type \\\" instead!)\r\nWhat is the difference between\r\na ' and a \"?  Or between a \" and a \\\"?\r\nOne is what we see when we're typing our\r\nprogram. The other is what appears on the \n\"console.\"\n");
     }
 }
+*/
