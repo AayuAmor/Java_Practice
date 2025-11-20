8144b24f76c3bc43950165d02596fbf8444aad0c
AayuAmor <aayuamor@gmail.com>
2025-11-20 06:33:07 +0545

    Add ConStatement and ForArray programs; update AddTwoNumbers and Hello.java formatting

---
+++ ConStatement.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class ConStatement{
+
+    Scanner sc = new Scanner(System.in);
+
+    void ageViwer(){
+        System.out.print("Enter Age:");
+        int age = sc.nextInt();
+        
+        
+        if (age < 18){
+            System.out.println("Minor");
+        }
+        else if(age>=18 && age<=60){
+            System.out.println("Adult");
+
+        }
+        else{
+            System.out.println("Senior Citizen");
+        };
+
+    };
+
+    void calculator(){
+        System.out.print("Enter Num1:");
+        int num1 = sc.nextInt();
+        System.out.print("\nEnter Num2:");
+        int num2 = sc.nextInt();
+        System.out.print("Enter Operator (+,-,*,/):");
+        String operator = sc.next();
+        
+
+        switch(operator){
+
+            case "+" : System.out.println(num1 + num2);
+            break;
+            
+            case "-": System.out.println(num1 - num2);
+            break;
+
+            case "*": System.out.println(num1 * num2);
+            break;
+
+            case "/": System.out.println(num1 / num2);
+            break;
+
+
+        }
+
+    }
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+    
+    
+        System.out.println("Select One:");
+        System.out.println("1. Age-Comparision ");
+        System.out.println("2. Calculator");
+        System.out.print("Your Input:");
+        int input = sc.nextInt();
+        
+
+        ConStatement condition = new ConStatement();
+
+        switch (input) {
+            case 1:
+                condition.ageViwer();
+                break;
+        
+            case 2:
+                condition.calculator();
+                break;
+        }
+        sc.close();
+
+    }
+};
---
+++ ForArray.java
@@ -0,0 +1,17 @@
+public class ForArray {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 43;
+        marks[1] = 58;
+        marks[2] = 98;
+        /* //Using index one by one
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        */
+        //Using Loop
+        for (int i = 0;i<3; i++ ){
+            System.out.println(marks[i]);
+        }
+    }
+}
